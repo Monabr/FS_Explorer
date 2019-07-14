80f9790c64dc2f63d83f18d7a7c2c03e320533c6
Sanchos <Monabr>
2019-07-15 02:22:47 +0300

    feat: иконки теперь есть bug: раскрытия теперь нет

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,6 +19,34 @@ using System.Windows.Shapes;
 
 namespace FS_Explorer
 {
+
+    public class ConvertBitmapToBitmapImage
+    {
+        /// <summary>
+        /// Takes a bitmap and converts it to an image that can be handled by WPF ImageBrush
+        /// </summary>
+        /// <param name="src">A bitmap image</param>
+        /// <returns>The image as a BitmapImage for WPF</returns>
+        public BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+
+    public class MyFileInfo
+    {
+        public ImageSource Icon { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+    }
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -25,6 +55,9 @@ namespace FS_Explorer
         public MainWindow()
         {
             InitializeComponent();
+            var sf = (ShellFolder)ShellObject.FromParsingName(
+                            "shell:::{4234d49b-0245-4df3-b780-3893943456e1}");
+            Debug.WriteLine(sf.Name);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -38,29 +71,57 @@ namespace FS_Explorer
         private void Drivers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
+
             Folders.Items.Clear();
    
 
             foreach (DirectoryInfo dir in new DirectoryInfo(Drivers.SelectedItem.ToString()).GetDirectories().OrderBy(x => x.Name).ToArray())
             {
-                TreeViewItem newItem = new TreeViewItem
-                {
-                    Tag = dir,
-                    Header = dir.ToString()
-                };
-                newItem.Items.Add("*");
+                //ShellFile shellFile = ShellFile.FromFilePath(dir.FullName.ToString());
+                ShellObject shellFolder = ShellObject.FromParsingName(dir.FullName.ToString());
+                BitmapSource shellThumb = shellFolder.Thumbnail.SmallBitmapSource;
+
+
+                //System.Drawing.Icon icon = (System.Drawing.Icon)System.Drawing.Icon.ExtractAssociatedIcon(dir.FullName);
+                //TreeViewItem newItem = new TreeViewItem
+                //{
+                //    Tag = dir,
+                //    Header = dir.ToString()
+                //};
+
+
+                MyFileInfo newItem = 
+                    new MyFileInfo
+                    {
+                        Name = dir.ToString(),
+                        Icon = shellThumb,
+                        FullName = dir.FullName
+                    }
+                    ;
+
+                //newItem.Items.Add("*");
                 Folders.Items.Add(newItem);
             }
 
             
-            foreach (FileInfo file in new DirectoryInfo(Drivers.SelectedItem.ToString()).GetFiles())
+            foreach (FileInfo file in new DirectoryInfo(Drivers.SelectedItem.ToString()).GetFiles().OrderBy(x => x.Name).ToArray())
             {
-                TreeViewItem newItem = new TreeViewItem
-                {
-                    Tag = file,
-                    Header = file.ToString()
-                };
-                Folders.Items.Add(newItem);
+
+                System.Drawing.Icon icon = (System.Drawing.Icon)System.Drawing.Icon.ExtractAssociatedIcon(file.FullName.ToString());
+                //TreeViewItem newItem = new TreeViewItem 
+                //{
+                //    Tag = file,
+                //    Header = file.ToString()
+
+                //};
+                Folders.Items.Add(
+                    new MyFileInfo
+                    {
+                        Name = file.ToString(),
+                        Icon = icon.ToImageSource(),
+                        FullName = file.FullName.ToString()
+                    }
+                    );
             }
 
 
@@ -97,6 +158,17 @@ namespace FS_Explorer
                     newItem.Items.Add("*");
                     item.Items.Add(newItem);
                 }
+
+                foreach (FileInfo file in dir.GetFiles().OrderBy(x => x.Name).ToArray())
+                {
+                    TreeViewItem newItem = new TreeViewItem
+                    {
+                        Tag = file,
+                        Header = file.ToString()
+                    };
+                    item.Items.Add(newItem);
+                }
+
             }
             catch (Exception ex)
             {  }
@@ -126,5 +198,7 @@ namespace FS_Explorer
 
             //System.Diagnostics.Process.Start(dir.FullName);
         }
+
+       
     }
 }
---
+++ System/Drawing/Icon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace FS_Explorer
+{
+    internal static class IconUtilities
+    {
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern bool DeleteObject(IntPtr hObject);
+
+        public static ImageSource ToImageSource(this Icon icon)
+        {
+            Bitmap bitmap = icon.ToBitmap();
+            IntPtr hBitmap = bitmap.GetHbitmap();
+
+            ImageSource wpfBitmap = Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            if (!DeleteObject(hBitmap))
+            {
+                throw new Win32Exception();
+            }
+
+            return wpfBitmap;
+        }
+    }
+}
